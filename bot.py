2a110628ca0cb98774b25104d43964be11a21313
naebkinet2-eng <naebkinet2@gmail.com>
2026-04-10 23:52:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ def welcome_keyboard():
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(
             text="🎁УЗНАТЬ НАЛИЧИЕ И ЦЕНЫ",
-            url="https://t.me/Oldsiws?text=Привет!👋%20скинь%20актуальный%20прайс"
+            url="https://t.me/OldSiWs?text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82%21%20%F0%9F%91%8B%20%D1%81%D0%BA%D0%B8%D0%BD%D1%8C%20%D0%B0%D0%BA%D1%82%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9%20%D0%BF%D1%80%D0%B0%D0%B9%D1%81"
         )]
     ])
 
